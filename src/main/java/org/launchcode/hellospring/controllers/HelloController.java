0e63bf21da8e9fec8be71a54b374541f0d19ba6a
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-06 17:09:20 -0400

    Added Chapter example for dynamic templates

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
+//import ch.qos.logback.core.model.Model;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 
 /**
  * Created by Chris Bay
@@ -17,16 +21,21 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    //@ResponseBody
+    public String helloWithQueryParam(@RequestParam String name, Model model ) {
+        String greeting = "Hello," + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
+
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+   // @ResponseBody
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        //String greeting = "Hello," + name + "!";
+        model.addAttribute("greeting", "Hello," + name + "!");
+        return "hello";
     }
 
     // /hello/form

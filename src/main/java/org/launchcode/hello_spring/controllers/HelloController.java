e63ca8170fd201fa142e9b2fec6d56dc8e1e1ffe
Irina Sharapova <ir.ur.smirnova@gmail.com>
2024-10-06 15:54:06 -0400

    Added Thymeleaf template. Chapter example for hello/goodbye.

--- src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hello_spring/controllers/HelloController.java
@@ -4,36 +4,39 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
+//@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("{name}")
+    @ResponseBody
     public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
     public String helloForm() {
-        String html =
-                "<html>" +
-                        "<body>" +
-                        "<form method='get' action='/hello'>" +
-                        "<input type='text' name='name' />" +
-                        "<input type='submit' value='Greet Me!' />" +
-                        "</form>" +
-                        "</body>" +
-                        "</html>";
-        return html;
+       // String html =
+//                "<html>" +
+//                        "<body>" +
+//                        "<form method='get' action='/hello'>" +
+//                        "<input type='text' name='name' />" +
+//                        "<input type='submit' value='Greet Me!' />" +
+//                        "</form>" +
+//                        "</body>" +
+//                        "</html>";
+        return "form";
     }
 }
